08d0932b3ee700ab96c84bfe163d702a0b68d9c8
Steven Dake <sdake@computelify.ai>
2023-01-17 16:01:25 -0700

    Tune the autogeneration a bit (#35)
    The autogeneration of the CUDA header files may have had too many
    allowed variables.
    
    Remove comments of old allow lists

--- build-cuda-driver.rs
+++ build-cuda-driver.rs
@@ -3,28 +3,43 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+use bindgen::callbacks::*;
+
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
 // Install NVIDIA CUDA prior to building the bindings with `cargo build`.
 // https://docs.rs/bindgen/latest/bindgen/struct.Builder.html
 // TODO(sdake): I am hopeful we can autogenerate network objects as well via:
 //              https://docs.rs/bindgen/latest/bindgen/callbacks/trait.ParseCallbacks.html
-// TODO(sdake): Pretty sure we need somethign like this: .allowlist_type("^cuuint(32|64)_t)"
 // TODO(sdake): We may want some variant of: .rustified_enum<T: AsRef<str>>(self, arg: T) -> Builder
+
+#[derive(Debug)]
+struct NetworkEmitter {}
+
+impl ParseCallbacks for NetworkEmitter {
+    fn generated_name_override(&self, _item_info: ItemInfo<'_>) -> Option<String> {
+ //        p!("f {:#?}\n", item_info.name);
+         None
+    }
+
+    fn item_name(&self, _name: &str) -> Option<String> {
+//        p!("v {}\n", name);
+        None
+    }
+}
 
 fn main() {
     let bindings = bindgen::Builder::default()
         .header("/usr/local/cuda-11.8/include/cuda.h")
-        .allowlist_type("^CU.*")
-        .allowlist_type("^cudaError_enum")
-        .allowlist_type("^cu.*Complex$")
-        .allowlist_type("^cuda.*")
-        .allowlist_type("^libraryPropertyTYpe.*")
-        .allowlist_var("^CU.*")
-        .allowlist_function("^cu.*")
-        .derive_default(true)
+        .allowlist_function("cu.*")
+        .allowlist_type("CU.*")
         .derive_eq(true)
-        .derive_hash(true)
-        .derive_ord(true)
         .array_pointers_in_arguments(true)
+        .parse_callbacks(Box::new(NetworkEmitter {}))
         .generate()
         .unwrap();
 
@@ -32,8 +47,9 @@ fn main() {
     bindings
         .write_to_file(target_path.join("cuda_driver_bindings.rs"))
         .expect("Couldn't write bindings!");
-    println!(
-        "Wrote bindings to {}\n",
+
+    p!(
+        "Wrote bindings to {}",
         target_path.join("cuda_driver_bindings.rs").display()
-    )
+    );
 }
